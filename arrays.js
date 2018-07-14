18fc87224e65d08deade5b262a28b902adb75f21
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-14 20:56:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray(array, element)
   array.push("foo")
   return array
 }
-function accessElementInArray(array)
+function accessElementInArray(array, index)
 {
-  
+  return array[index]
 }
