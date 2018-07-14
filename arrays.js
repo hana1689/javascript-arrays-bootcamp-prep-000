30461eec3c3a80240a6ce4dad2961a9dbaca8f43
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-14 21:06:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,4 +39,8 @@ function destructivelyRemoveElementFromEndOfArray(array)
   array.pop()
   return array
 }
-function 
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array = array.slice(0, array.length - 1)
+  return array
+}
